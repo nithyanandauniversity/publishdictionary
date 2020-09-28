2a480e12216b62090be3e8d455177a223f29c481
Akhil Kodali <50161+akhil@users.noreply.github.com>
2020-09-28 19:55:50 -0400

    added more fixes for dict markup

--- src/main/scala/MyWebServer.scala
+++ src/main/scala/MyWebServer.scala
@@ -8,11 +8,11 @@ object MyWebServer extends cask.MainRoutes {
 
   @cask.get("/word/:word")
   def getMeaning(word: String): cask.Response[String] = {
-    val w = myDict.getWords(word)
+    val w = myDict.getWords(word).sortBy(_.source)
     cask.Response(
-      headers = Seq(("Content-Type", "text/html; charset=UTF-8")),
-      data = w.map(MyParseXml.getHtml).mkString("\n")
-
+      //headers = Seq(("Content-Type", "text/html; charset=UTF-8")),
+      data =
+        s"=== ${w.head.word} ===\n\r" + w.map(MyParseXml.getHtml).mkString("\n\r")
     )
   }
 
@@ -28,15 +28,22 @@ object MyParseXml {
   private val dictMap: Map[String, String] = myDict.dictionariesList.map(d => d.id -> d.name).toMap
 
   def getHtml(w: Data): String =
-    <html>
-      <body>{dictMap(w.source)}<br/></body>
-      <body>{w.word}<br/></body>
-      {MyParseXml.process(w.meaning)}
-      <hr/>
-    </html>
-    .mkString
-      .replace("<s>", "<strong>")
-      .replace("</s>", "</strong>")
+    s"""<H1>${dictMap(w.source)}</H1>
+       |${MyParseXml.process(w.meaning)}
+       |<br />
+       |""".stripMargin
+      .mkString
+      .replace("<H1>", "=== ")
+      .replace("</H1>", " ===")
+      .replace("<s>", "'''")
+      .replace("<i>", "''")
+      .replace("</i>", "''")
+      .replace("</s>", "'''")
+      .replace("<lb/>", "\n\r")
+      .replace("<br/>", "\n\r")
+      .replace("<body>", "\n")
+      .replace("</body>", "\n")
+      .stripMargin
 }
 
 case class Data(word: String, lnum: String, meaning: String, source: String)
@@ -45,7 +52,7 @@ case class Dictionaries(id: String, date: Int, name: String)
 class MyDict {
 
   import io.getquill._
-  lazy val ctx = new MysqlJdbcContext(SnakeCase, "ctx")
+  val ctx = new MysqlJdbcContext(SnakeCase, "ctx")
   import ctx._
 
   private def words(word: String) = quote {
