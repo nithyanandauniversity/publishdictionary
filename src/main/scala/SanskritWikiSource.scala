4420a75f35ac89eecd4171fe975c21e65ec2b7dd
Akhil Kodali <50161+akhil@users.noreply.github.com>
2020-10-06 02:23:02 -0400

    tested upload by url

--- src/main/scala/SanskritWikiSource.scala
+++ src/main/scala/SanskritWikiSource.scala
@@ -13,12 +13,13 @@ object SanskritWikiSource extends App {
 
   private val wiki = new Wiki.Builder()
     .withApiEndpoint(
-      HttpUrl.parse("https://sa.wikisource.org/w/api.php")
+      HttpUrl.parse("https://test.hinduismpedia.org/api.php")
     )
     //.withDebug(true)
-    .withLogin("Dashsant@swamiji1", "qegj7n83pfg9k7tukjjjpjhc3iutspih")
+    //.withLogin("Dashsant@swamiji1", "qegj7n83pfg9k7tukjjjpjhc3iutspih")
+    .withLogin("Admin@test123", "ma52illci70ei31mqimc808to24ud61n")
     .build()
-
+/*
   val page = wiki.getRandomPages(1).get(0)
 
   val transliterators = Transliterator.getInstance("Any-Eng")
@@ -36,5 +37,11 @@ object SanskritWikiSource extends App {
   println(b)
   println(b.size)
   println(transliterators.transform(myCategory))
+*/
 
+  val book = "संस्काररत्नमाला (भागः १).djvu"
+  val bo = wiki.uploadByUrl(
+    HttpUrl.parse(s"https://upload.wikimedia.org/wikipedia/commons/f/f8/%E0%A4%B8%E0%A4%82%E0%A4%B8%E0%A5%8D%E0%A4%95%E0%A4%BE%E0%A4%B0%E0%A4%B0%E0%A4%A4%E0%A5%8D%E0%A4%A8%E0%A4%AE%E0%A4%BE%E0%A4%B2%E0%A4%BE_%28%E0%A4%AD%E0%A4%BE%E0%A4%97%E0%A4%83_%E0%A5%A7%29.djvu"),
+    "संस्काररत्नमाला (भागः १).djvu", book, "uploaded from sa.wikisource.org")
+  println(bo)
 }
